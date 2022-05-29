1a49ae87212799be8fab12c5d201b6cb371a5d38
stefangeneralao <stefangeneralao@gmail.com>
2022-05-29 20:52:02 +0200

    Update docker repository target to private registry

--- app/scripts/docker/push.sh
+++ app/scripts/docker/push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yarn
 yarn build
-docker buildx build --platform linux/arm/v6,linux/arm/v7,linux/amd64 --push -t stefangeneralao/stilipshue-frontend .
+docker buildx build --platform linux/arm/v6,linux/arm/v7,linux/amd64 --push -t docker.stefangeneralao.com/stilipshue-frontend .
--- backend/scripts/docker/push.sh
+++ backend/scripts/docker/push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yarn
 yarn build
-docker buildx build --platform linux/arm/v7,linux/amd64 --push -t stefangeneralao/stilipshue-backend .
+docker buildx build --platform linux/arm/v6,linux/arm/v7,linux/amd64 --push -t docker.stefangeneralao.com/stilipshue-backend .
