34838846fc80e5a0f49d12d13fc642eecb84d5ad
stefangeneralao <stefangeneralao@gmail.com>
2022-09-16 14:17:00 +0200

    Update jobs preset

--- backend/src/jobs/JobsPreset.ts
+++ backend/src/jobs/JobsPreset.ts
@@ -1,55 +1,60 @@
 import { Job } from './Job';
 import { Jobs } from './Jobs';
 import { defaultRelays } from '../relays';
-import { Tasks } from '../tasks/Tasks';
+import { TurnOffRelayTask, TurnOnRelayTask } from '../tasks/Task';
 
 const [bedroom1, bedroom2] = defaultRelays.findByTags(['bedroom']);
 const [windowPlant] = defaultRelays.findByTags(['plant']);
 const [mushroom] = defaultRelays.findByTags(['mushroom']);
 const [ledString] = defaultRelays.findByTags(['led-string']);
-const [miner] = defaultRelays.findByTags(['miner']);
+const [miner] = defaultRelays.findByName('miner');
 
 const jobs = new Jobs()
   .addJob(
     new Job()
       .setId('Miner on')
       .setRule({ hour: 6, minute: 0 })
-      .addTask(new (Tasks.getAllTasks().turnOnRelay)(miner))
+      .addTask(new TurnOnRelayTask(miner))
   )
   .addJob(
     new Job()
       .setId('Plant lamps on')
       .setRule({ hour: 6, minute: 0 })
-      .addTask(new (Tasks.getAllTasks().turnOnRelay)(windowPlant))
+      .addTask(new TurnOnRelayTask(windowPlant))
   )
   .addJob(
     new Job()
       .setId('Lights on')
       .setRule({ hour: 7, minute: 0 })
-      .addTask(new (Tasks.getAllTasks().turnOnRelay)(bedroom1))
-      .addTask(new (Tasks.getAllTasks().turnOnRelay)(bedroom2))
-      .addTask(new (Tasks.getAllTasks().turnOnRelay)(mushroom))
-      .addTask(new (Tasks.getAllTasks().turnOnRelay)(ledString))
+      .addTask(new TurnOnRelayTask(bedroom1))
+      .addTask(new TurnOnRelayTask(bedroom2))
+      .addTask(new TurnOnRelayTask(mushroom))
+      .addTask(new TurnOnRelayTask(ledString))
   )
   .addJob(
     new Job()
       .setId('Plant lamps off')
       .setRule({ hour: 18, minute: 0 })
-      .addTask(new (Tasks.getAllTasks().turnOffRelay)(windowPlant))
+      .addTask(new TurnOffRelayTask(windowPlant))
   )
   .addJob(
     new Job()
       .setId('Mushroom light off')
+      .setRule({ hour: 18, minute: 0 })
+      .addTask(new TurnOffRelayTask(mushroom))
+  )
+  .addJob(
+    new Job()
+      .setId('Bedroom lights off')
       .setRule({ hour: 21, minute: 0 })
-      .addTask(new (Tasks.getAllTasks().turnOffRelay)(mushroom))
+      .addTask(new TurnOffRelayTask(bedroom1))
+      .addTask(new TurnOffRelayTask(bedroom2))
   )
   .addJob(
     new Job()
       .setId('Cozy lights off')
       .setRule({ hour: 23, minute: 0 })
-      .addTask(new (Tasks.getAllTasks().turnOffRelay)(bedroom1))
-      .addTask(new (Tasks.getAllTasks().turnOffRelay)(bedroom2))
-      .addTask(new (Tasks.getAllTasks().turnOffRelay)(ledString))
+      .addTask(new TurnOffRelayTask(ledString))
   );
 
 export { jobs };
